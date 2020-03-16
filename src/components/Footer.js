d633378a7e58a5521e1e4dde9a46ae81280b358c
Sushant11 <sushant.chitrakar@citytech.global>
2020-03-16 18:06:06 +0545

    Initial

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <span>Show:</span>
+            
+        </div>
+    );
+};
+
+export default Footer;
